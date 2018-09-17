aaf9d2b7804d625ddfe1a561d623a038a2ed9e2c
nikola27 <nikolica.saric@hotmail.com>
2018-09-17 14:22:02 +0200

    male izmjene

--- Odrzavanje/predlozak/head.php
+++ Odrzavanje/predlozak/head.php
@@ -1,18 +1,20 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $nazivAPP; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"type="text/css" href="<?php echo $putanjaAPP; ?>css/foundation.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $putanjaAPP; ?>css/app.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $putanja ?>css/responsive-tables.css">
+    
     <meta name="og:title" content="Održavanje"/>
     <meta name="og:type" content="Repair and maintenance"/>
     <meta name="og:url" content="http://www.nikolasaric.byethost13.com"/>
     <meta name="og:site_name" content="Održavanje"/>
     <meta name="og:description" content="Repair and maintenance of residential buildings and houses"/>
+    
     <link href="<?php echo $putanjaAPP; ?>css/fontawesome/css/fontawesome-all.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo $putanjaAPP; ?>css/foundation.css">
-    <link rel="stylesheet" href="<?php echo $putanjaAPP; ?>css/app.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" 
-    integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" 
-    crossorigin="anonymous">
+    integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <link rel="shortcut icon"type="image/png" href="<?php echo $putanjaAPP; ?>img/logo1.png">
     
 
--- Odrzavanje/predlozak/podnozje.php
+++ Odrzavanje/predlozak/podnozje.php
@@ -1,2 +1,3 @@
 <hr />
 &copy; <?php echo date("Y") ?> Održavanje
+<a href="https://github.com/nikola27/odrzavanje" target="_blank" style="text-decorations:none; color:inherit;"><i class="fab fa-github fa-2x"></i></a>
