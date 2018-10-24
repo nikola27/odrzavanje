006804f6d26092657ca976f21bc7d89492c99492
nikola27 <nikolica.saric@hotmail.com>
2018-10-24 09:36:50 +0200

    nadzorna, dijagram

--- Odrzavanje/privatno/nadzornaPloca.php
+++ Odrzavanje/privatno/nadzornaPloca.php
@@ -17,7 +17,70 @@ if(!isset($_SESSION[$idAPP."o"])){
     <?php include_once "../predlozak/izbornik.php" ?>
 
 
-  	Nadzorna ploča
+  	<div id="container" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto">
+</div>
+
+
+
+
+ <?php include_once "../predlozak/podnozje.php" ?>
+  </div>
+  
+    
+    
+
+   <?php include_once "../predlozak/podnozje.php" ?>
+  <?php include_once "../predlozak/skripte.php" ?>
+  <script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="https://code.highcharts.com/modules/export-data.js"></script>
+<script>
+  Highcharts.chart('container', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Broj kvarova po kategorijama'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.y}</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.y}',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'kvar',
+        colorByPoint: true,
+        data: 
+          <?php 
+            
+            $izraz = $veza->prepare("
+ 
+            select count(a.sifra)as y,
+            b.naziv as name
+             from kvar a left join kategorija b on a.kategorija=b.sifra group by kategorija
+  ");
+  $izraz->execute();
+ $rezultati = $izraz->fetchAll(PDO::FETCH_OBJ);
+            echo json_encode($rezultati, JSON_NUMERIC_CHECK);
+            ?>
+         
+    }]
+});
+</script> 
     
 
     <?php include_once "../predlozak/podnozje.php" ?>
